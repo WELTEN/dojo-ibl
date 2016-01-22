ad340dfc0d95e972f7a7c5136c0cac318787ba7f
Titogelo <titogelo@gmail.com>
2016-01-22 15:47:35 +0100

    hide super user from API

--- src/main/java/org/celstec/arlearn2/api/AccountApi.java
+++ src/main/java/org/celstec/arlearn2/api/AccountApi.java
@@ -112,27 +112,27 @@ public class AccountApi extends Service {
 		return serialise(ad.getContactDetails(accountFullId), accept);
 	}
 	
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	@CacheControlHeader("no-cache")
-	@Path("/makesuper/{accountFullId}")
-	public String makesuper(@HeaderParam("Authorization") String token,
-			@DefaultValue("application/json") @HeaderParam("Accept") String accept,
-			@PathParam("accountFullId") String accountFullId
-
-			) {
-		if (!validCredentials(token))
-			return serialise(getInvalidCredentialsBean(), accept);
-
-		AccountDelegator ad = new AccountDelegator(this);
-		String myAccount = UserLoggedInManager.getUser(token);
-		if (myAccount == null) {
-			Account ac = new Account();
-			ac.setError("account is not logged in");
-		}
-		ad.makeSuper(accountFullId);
-		return "{}";
-	}
+//	@GET
+//	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+//	@CacheControlHeader("no-cache")
+//	@Path("/makesuper/{accountFullId}")
+//	public String makesuper(@HeaderParam("Authorization") String token,
+//			@DefaultValue("application/json") @HeaderParam("Accept") String accept,
+//			@PathParam("accountFullId") String accountFullId
+//
+//			) {
+//		if (!validCredentials(token))
+//			return serialise(getInvalidCredentialsBean(), accept);
+//
+//		AccountDelegator ad = new AccountDelegator(this);
+//		String myAccount = UserLoggedInManager.getUser(token);
+//		if (myAccount == null) {
+//			Account ac = new Account();
+//			ac.setError("account is not logged in");
+//		}
+//		ad.makeSuper(accountFullId);
+//		return "{}";
+//	}
 
     private static final Logger log = Logger.getLogger(AccountApi.class.getName());
 
