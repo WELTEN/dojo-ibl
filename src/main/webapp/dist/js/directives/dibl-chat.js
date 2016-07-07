3eb2c858d54105cb0761b463a0c7170e86a11118
Titogelo <titogelo@gmail.com>
2016-07-07 10:00:39 +0200

    fixed #125 profile picture and name brings user to the profile

--- src/main/webapp/dist/js/directives/dibl-chat.js
+++ src/main/webapp/dist/js/directives/dibl-chat.js
@@ -45,37 +45,6 @@ angular.module('DojoIBL')
                 scope.userName = UserService.getUserFromCache(scope.message.senderId).name;
                 scope.avatar = UserService.getUserFromCache(scope.message.senderId).picture;
 
-                //if(scope.response.responseValue.indexOf("{") == -1){
-                //    scope.extension = "text";
-                //    scope.resource = scope.response.responseValue;
-                //}else{
-                //    var json = JSON.parse(scope.response.responseValue);
-                //
-                //    if (json.pdfUrl){
-                //        scope.type = json.fileType;
-                //        scope.name = json.fileName;
-                //        scope.extension = "pdf"
-                //        scope.resource = json.pdfUrl;
-                //    }
-                //    if (json.videoUrl){
-                //        scope.type = json.fileType;
-                //        scope.name = json.fileName;
-                //        scope.extension = "video"
-                //        scope.resource = json.videoUrl;
-                //    }
-                //    if (json.imageUrl){
-                //        scope.type = json.fileType;
-                //        scope.name = json.fileName;
-                //        scope.extension = "image"
-                //        scope.resource = json.imageUrl;
-                //    }
-                //    if (json.audioUrl){
-                //        scope.type = json.fileType;
-                //        scope.name = json.fileName;
-                //        scope.extension = "audio"
-                //        scope.resource = json.audioUrl;
-                //    }
-                //}
             }
         };
     })
