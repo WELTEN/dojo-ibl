487c3721f7fa7a42a22580fb97a433387e7b3f7f
Titogelo <titogelo@gmail.com>
2016-07-13 19:21:23 +0200

    fixed problems in chat

--- src/main/webapp/dist/js/directives/dibl-chat.js
+++ src/main/webapp/dist/js/directives/dibl-chat.js
@@ -1,21 +1,22 @@
 angular.module('DojoIBL')
 
-    //.directive('chat', function($window) {
-    //    return  {
-    //        restrict: 'A',
-    //        link: function (scope, element, attrs, controller) {
-    //            scope.$watch(
-    //                function () {
-    //                    return element[0].childNodes.length;
-    //                },
-    //                function (newValue, oldValue) {
-    //                    if (newValue !== oldValue) {
-    //                        element.scrollTop(element[0].scrollHeight);
-    //                    }
-    //                });
-    //        }
-    //    };
-    //})
+    .directive('chat', function($window) {
+        return  {
+            restrict: 'A',
+            link: function (scope, element, attrs, controller) {
+                scope.$watch(
+                    function () {
+                        return element[0].childNodes.length;
+                    },
+                    function (newValue, oldValue) {
+                        if (newValue !== oldValue) {
+                            console.log("scroll")
+                            element.scrollTop(element[0].scrollHeight);
+                        }
+                    });
+            }
+        };
+    })
 
     .directive('ngEnter', function() {
         return {
