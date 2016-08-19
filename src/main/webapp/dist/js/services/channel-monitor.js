0b222818c105dc2f5634a647c583b60efc4ecbb9
Titogelo <titogelo@gmail.com>
2016-08-19 14:00:07 +0200

    fixed #161 optimizing creating channels in GCM

--- src/main/webapp/dist/js/services/channel-monitor.js
+++ src/main/webapp/dist/js/services/channel-monitor.js
@@ -31,54 +31,6 @@ angular.module('DojoIBL')
                 };
 
                 var context = this;
-                //this.socketCreationCallback = function (channelData) {
-                //    var channel = new goog.appengine.Channel(channelData.token);
-                //
-                //    console.log(channelData.channelId)
-                //
-                //    context.channelId = channelData.channelId;
-                //    var socket = channel.open();
-                //    socket.onerror = function () {
-                //        //console.log("Channel error");
-                //    };
-                //    socket.onclose = function () {
-                //
-                //        //console.log("Channel closed, reopening");
-                //        //We reopen the channel
-                //        context.messageCallback = context.channelSocket.onmessage;
-                //        context.channelSocket = undefined;
-                //        $.getJSON("chats/channel", context.socketCreationCallback);
-                //
-                //        console.warn("Channel closed, reopening");
-                //        //We reopen the channel
-                //        context.messageCallback = context.channelSocket.onmessage;
-                //        context.channelSocket = undefined;
-                //        $.getJSON("chats/channel", context.socketCreationCallback);
-                //        dataCache.remove(user.accountType+":"+user.localId);
-                //        console.warn('Channel closed.');
-                //
-                //        swal({
-                //            title: "Timeout!",
-                //            text: "The session has timed out. Refresh!",
-                //            type: "warning",
-                //            showCancelButton: false,
-                //            confirmButtonColor: "#DD6B55",
-                //            confirmButtonText: "Refresh!!",
-                //            closeOnConfirm: false
-                //        }, function () {
-                //            //window.location.replace(window.location.href);
-                //            location.reload();
-                //            //swal("Timeout!", "Refresh this page to continue.", "success");
-                //        });
-                //    };
-                //    context.channelSocket = socket;
-                //    //console.info("Channel info received");
-                //    context.channelSocket.onmessage = context.messageCallback;
-                //};
-                //
-                //$http({url: "/rest/channelAPI/token", method: 'GET'}).success(this.socketCreationCallback);
-
-                // Option 2
 
                 this.setSocketChannel = function (channelData) {
                     var channel = new goog.appengine.Channel(channelData.token);
@@ -92,19 +44,14 @@ angular.module('DojoIBL')
                     };
                     socket.onclose = function () {
 
-                        //console.log("Channel closed, reopening");
                         //We reopen the channel
                         context.messageCallback = context.channelSocket.onmessage;
                         context.channelSocket = undefined;
                         $.getJSON("chats/channel", context.socketCreationCallback);
 
-                        console.warn("Channel closed, reopening");
-                        //We reopen the channel
-                        context.messageCallback = context.channelSocket.onmessage;
-                        context.channelSocket = undefined;
-                        $.getJSON("chats/channel", context.socketCreationCallback);
                         dataCache.remove(user.accountType+":"+user.localId);
-                        console.warn('Channel closed.');
+
+                        console.warn('Channel closed');
 
                         swal({
                             title: "Timeout!",
@@ -128,99 +75,11 @@ angular.module('DojoIBL')
                 if (!dataCache.get(user.accountType+":"+user.localId)) {
                     $http({url: "/rest/channelAPI/token", method: 'GET'}).success(function(channelData){
                         context.setSocketChannel(channelData);
-                        console.info("Ask for a new token", "-1 channel created")
                     });
                 }else {
                     var channelData = dataCache.get(user.accountType + ":" + user.localId);
                     context.setSocketChannel(channelData);
-                    console.info("Reuse the old token", channelData.token)
-
                 }
-
-
-
-                //var context = this;
-                //this.socketCreationCallback = function (channelData) {
-                //
-                //    var channel = new goog.appengine.Channel(channelData.token);
-                //
-                //    dataCache.put(user.accountType+":"+user.localId, channelData);
-                //
-                //    context.channelId = channelData.channelId;
-                //
-                //    var socket = channel.open();
-                //
-                //    socket.onerror = function () {
-                //        console.log("Channel error");
-                //    };
-                //
-                //    socket.onclose = function () {
-                //        console.log("Channel closed, reopening");
-                //        //We reopen the channel
-                //        context.messageCallback = context.channelSocket.onmessage;
-                //        context.channelSocket = undefined;
-                //        $.getJSON("chats/channel", context.socketCreationCallback);
-                //    };
-                //
-                //    context.channelSocket = socket;
-                //    //console.log("Channel info received");
-                //    //console.log(channelData.channelId);
-                //    context.channelSocket.onmessage = context.messageCallback;
-                //};
-                //
-                //
-                //if (!dataCache.get(user.accountType+":"+user.localId)) {
-                //    $http({url: "/rest/channelAPI/token", method: 'GET'}).success(this.socketCreationCallback);
-                //}else{
-                //    var channelData = dataCache.get(user.accountType+":"+user.localId);
-                //
-                //    var channel = new goog.appengine.Channel(channelData.token);
-                //    context.channelId = channelData.channelId;
-                //
-                //
-                //    var socket = channel.open();
-                //
-                //    socket.onopen = function(){
-                //        console.warn("Channel opened");
-                //
-                //    };
-                //
-                //    socket.onmessage = function(){
-                //        console.warn("Channel opened");
-                //
-                //    };
-                //
-                //    socket.onerror = function () {
-                //        console.warn("Channel error");
-                //    };
-                //
-                //    socket.onclose = function () {
-                //        console.warn("Channel closed, reopening");
-                //        //We reopen the channel
-                //        context.messageCallback = context.channelSocket.onmessage;
-                //        context.channelSocket = undefined;
-                //        $.getJSON("chats/channel", context.socketCreationCallback);
-                //        dataCache.remove(user.accountType+":"+user.localId);
-                //        console.warn('Channel closed.');
-                //
-                //        swal({
-                //            title: "Timeout!",
-                //            text: "The session has timed out. Refresh!",
-                //            type: "warning",
-                //            showCancelButton: false,
-                //            confirmButtonColor: "#DD6B55",
-                //            confirmButtonText: "Refresh!!",
-                //            closeOnConfirm: false
-                //        }, function () {
-                //            //window.location.replace(window.location.href);
-                //            location.reload();
-                //            //swal("Timeout!", "Refresh this page to continue.", "success");
-                //        });
-                //    };
-                //
-                //    context.channelSocket = socket;
-                //    context.channelSocket.onmessage = context.messageCallback;
-                //}
             }
         }
     }
