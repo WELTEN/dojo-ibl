92822e74e0f1a42305f8b6131808efaa0faf5dbf
Titogelo <titogelo@gmail.com>
2017-09-02 20:00:10 +0200

    began with timeline 40% done
    the main challenge here is to show run > activities > responses

--- src/main/webapp/src/components/run/timeline.controller.js
+++ src/main/webapp/src/components/run/timeline.controller.js
@@ -1,13 +1,60 @@
 angular.module('DojoIBL')
 
+    //.factory("$infiniteScroll", ["$firebaseArray", "$rootScope",
+    //    function ($firebaseArray, $rootScope) {
+    //        return function (ref, pageMax) {
+    //            var main;
+    //            var current = pageMax;
+    //            console.log(ref.path.toString());
+    //            console.log(ref.database.ref().child(ref.path.toString()));
+    //            var result = $firebaseArray(ref.database.ref().child(ref.path.toString()).limitToFirst(pageMax));
+    //            result.$scroll = function () {
+    //                if (result.length < current) return false;
+    //                if (main) {
+    //                    main.off("child_added");
+    //                    main.off("child_removed");
+    //                    main.off("child_changed");
+    //                    main.off("child_moved");
+    //                }
+    //                current += pageMax;
+    //                console.log(current);
+    //                main = ref.database.ref().child(ref.path.toString()).limitToFirst(current);
+    //                main.on("child_added", function ($snapshot, $prev) { if (result.$$added($snapshot, $prev)) { result.$$process("child_added", result.$$added($snapshot, $prev), $prev); $rootScope.$digest(); } });
+    //                main.on("child_removed", function ($snapshot, $prev) { if (result.$$removed($snapshot)) { result.$$process("child_removed", result.$getRecord($snapshot.key)); $rootScope.$digest(); } });
+    //                main.on("child_changed", function ($snapshot, $prev) { if (result.$$updated($snapshot)) { result.$$process("child_changed", result.$getRecord($snapshot.key)); $rootScope.$digest(); } });
+    //                main.on("child_moved", function ($snapshot, $prev) { if (result.$$moved($snapshot, $prev)) { result.$$process("child_moved", result.$getRecord($snapshot.key), $prev); $rootScope.$digest(); } });
+    //            }
+    //
+    //            return result;
+    //        }
+    //    }
+    //])
     .controller('TimelineController', function ($scope, $sce, $stateParams, $state, Response,
                                                 ActivityService, UserService, GameService, RunService, AccountService,
-                                                ChannelService) {
+                                                ChannelService, $firebaseArray) {
 
-        $scope.games = {};
-        $scope.games.games = [];
+        var ctrl = this;
+        var responsesRef = firebase.database().ref("responses").child($stateParams.runId);
 
-        $scope.disableGameLoading = false;
+        ctrl.getResponses = function() {
+            $scope.responses = $firebaseArray(responsesRef);
+
+            angular.forEach($scope.responses, function(key, value){
+                console.log(key, value)
+            });
+
+            //$scope.responses = $firebaseArray(responsesRef.limitToLast($scope.pageSize * $scope.page));
+        };
+
+        //$scope.pageItems = $pageArray(responseValidRed, 'number');
+
+        ctrl.getResponses();
+
+        //
+        //$scope.games = {};
+        //$scope.games.games = [];
+        //
+        //$scope.disableGameLoading = false;
 
         var old_item_id = 0;
         var old_side_left = true;
@@ -49,69 +96,69 @@ angular.module('DojoIBL')
         //});
 
 
-        $scope.loadMoreGames = function () {
-
-            $scope.disableGameLoading = true;
-
-            Response.resume({resumptionToken: $scope.games.resumptionToken, runId: $stateParams.runId, from: 0 })
-                .$promise.then(function (data) {
-
-                    var responses = [];
-
-                    angular.forEach(data.responses, function(resp){
-
-                        // TODO check this is not working well
-                        if(resp.generalItemId != old_item_id){
-                            if(old_side_left){
-                                resp.move_right = false;
-                            }else{
-                                resp.move_right = true;
-                            }
-                        }
-
-                        old_item_id = resp.generalItemId;
-                        old_side_left = resp.move_right;
-
-                        RunService.getRunById($stateParams.runId).then(function (run) {
-                            ActivityService.getActivityById(resp.generalItemId, run.gameId).then(function(data){
-                                resp.activity = data;
-                            });
-                        });
-
-                        //UserService.getUserByAccount($stateParams.runId, resp.userEmail.split(':')[1]).then(function(data){
-                        //    resp.user = data;
-                        //});
-
-                        resp.user = UserService.getUser($stateParams.runId, resp.userEmail);
-
-                        responses.push(resp);
-                    });
-
-                    $scope.games.games = $scope.games.games.concat(responses);
-                    $scope.games.resumptionToken = data.resumptionToken;
-                    $scope.games.serverTime = data.serverTime;
-
-                    if (data.resumptionToken) {
-                        $scope.disableGameLoading = false
-                    } else {
-                        $scope.disableGameLoading = true
-                    }
-                });
-        };
-
-
-        UserService.getUsersForRun($stateParams.runId).then(function(data){
-
-            $scope.usersRun = data;
+        //$scope.loadMoreGames = function () {
+        //
+        //    $scope.disableGameLoading = true;
+        //
+        //    Response.resume({resumptionToken: $scope.games.resumptionToken, runId: $stateParams.runId, from: 0 })
+        //        .$promise.then(function (data) {
+        //
+        //            var responses = [];
+        //
+        //            angular.forEach(data.responses, function(resp){
+        //
+        //                // TODO check this is not working well
+        //                if(resp.generalItemId != old_item_id){
+        //                    if(old_side_left){
+        //                        resp.move_right = false;
+        //                    }else{
+        //                        resp.move_right = true;
+        //                    }
+        //                }
+        //
+        //                old_item_id = resp.generalItemId;
+        //                old_side_left = resp.move_right;
+        //
+        //                RunService.getRunById($stateParams.runId).then(function (run) {
+        //                    ActivityService.getActivityById(resp.generalItemId, run.gameId).then(function(data){
+        //                        resp.activity = data;
+        //                    });
+        //                });
+        //
+        //                //UserService.getUserByAccount($stateParams.runId, resp.userEmail.split(':')[1]).then(function(data){
+        //                //    resp.user = data;
+        //                //});
+        //
+        //                resp.user = UserService.getUser($stateParams.runId, resp.userEmail);
+        //
+        //                responses.push(resp);
+        //            });
+        //
+        //            $scope.games.games = $scope.games.games.concat(responses);
+        //            $scope.games.resumptionToken = data.resumptionToken;
+        //            $scope.games.serverTime = data.serverTime;
+        //
+        //            if (data.resumptionToken) {
+        //                $scope.disableGameLoading = false
+        //            } else {
+        //                $scope.disableGameLoading = true
+        //            }
+        //        });
+        //};
 
 
-        });
+        //UserService.getUsersForRun($stateParams.runId).then(function(data){
+        //
+        //    $scope.usersRun = data;
+        //
+        //
+        //});
 
         $scope.filter = {};
 
         // Functions - Definitions
         $scope.filterByCategory = function(response) {
-            return $scope.filter[response.user.localId] || noFilter($scope.filter);
+            return $scope.filter[response.name] || noFilter($scope.filter);
         };
 
         function noFilter(filterObj) {
