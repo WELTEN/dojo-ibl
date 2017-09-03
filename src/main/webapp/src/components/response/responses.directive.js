150f9cad58fd69005022288af22932739196430c
Titogelo <titogelo@gmail.com>
2017-09-03 13:12:07 +0200

    edit and remove response functionality

--- src/main/webapp/src/components/response/responses.directive.js
+++ src/main/webapp/src/components/response/responses.directive.js
@@ -10,6 +10,66 @@ angular.module('DojoIBL')
             templateUrl: '/src/components/response/response.template.html',
             link: function (scope, element, attrs) {
 
+                AccountService.myDetails().then(function(data){
+                    scope.myAccount = data;
+                });
+
+
+                scope.removeComment = function(data){
+
+                    var responsesRef = firebase.database().ref("responses").child(data.runId).child(data.generalItemId);
+
+                    swal({
+                        title: "Are you sure?",
+                        text: "You will not be able to recover it in the future!",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Yes, remove it!",
+                        closeOnConfirm: false
+                    }, function () {
+                        swal("Removed!", "Your contribution has been removed from the inquiry", "success");
+
+                        responsesRef.child(data.$id).remove(function(error){
+                            if (error) {
+                                console.log("Error:", error);
+                            } else {
+                                console.log("Removed successfully!");
+                            }
+                        });
+
+                    });
+                };
+
+                scope.newText = scope.response.responseValue;
+
+                    scope.saveComment = function(newValue) {
+
+                    var updates = {};
+
+                        updates['/responses/' + scope.response.runId + '/' + scope.response.generalItemId + '/' + scope.response.$id ] = {
+                            "type": "org.celstec.arlearn2.beans.run.Response",
+                            "runId": scope.response.runId,
+                            "deleted": scope.response.deleted,
+                            "generalItemId": scope.response.generalItemId,
+                            "userAccountType": scope.response.userAccountType,
+                            "userLocalId": scope.response.userLocalId,
+                            "userName": scope.response.userName,
+                            "userProfile": scope.response.userProfile,
+                            "responseValue": newValue,
+                            "parentId": scope.response.parentId,
+                            "revoked": scope.response.revoked,
+                            "lastModificationDate": firebase.database.ServerValue.TIMESTAMP
+                        };
+
+                        scope.newText = '';
+                        scope.hiddenDiv = false;
+
+                        return firebase.database().ref().update(updates);
+
+                };
+
+
                 //console.log(scope);
 
                 //scope.sendComment = function(responseParent, responseChildren) {
